d30e58ef7b36fa472054abd107e01d91c1c2e48f
dreaner <dreaner@dreaner-F8Va.(none)>
2013-05-14 00:59:56 +0800

    x

--- taobao.py
+++ taobao.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python
 import urllib2
 from bs4 import BeautifulSoup
-import chardet
+import re
+
+def getPriceTmall(url):
+	print url
+	html = urllib2.urlopen(url)
+	soup = BeautifulSoup(html)
+	try:
+		cls = soup.find(id="J_DealRecord")
+		divs = cls.div
+		cons = divs.contents
+		ul = cons[3]
+		li = ul.li
+		cons1 = li.contents
+		price = cons1[1].string
+	except:
+		price = -1
+	return 'tmall   ' + url + '    ' + price
+	#return price
+
+def getPriceTaobao(url):
+	html = urllib2.urlopen(url)
+	soup = BeautifulSoup(html)
+	try:
+		cls = soup.find(id="J_StrPrice")
+		cons = cls.contents;
+		price = cons[1].string
+		#name = soup.title.string
+	except:
+		price = '-1'
+
+	return 'taobao   ' + url + '    ' + price
+	#return price
 
 def getPrice(url):
 	html = urllib2.urlopen(url)
@@ -14,7 +45,6 @@ def getPrice(url):
 		#name = soup.title.string
 	except:
 		price = '-1'
-
 	return url + '    ' + price
 	#return price
 
@@ -24,10 +54,18 @@ result_file = open('taobao.out', 'w')
 result_file.write('    page-url    ' + '    ' + 'price' + '    \n')
 
 for url in url_file:
+	if not url:
+		continue
+
 	url = url.strip()
-	result = getPrice(url)
+	if url.startswith('http://item.taobao.com/'):
+		result = getPriceTaobao(url)
+	elif url.startswith('http://detail.tmall.com/'):
+		result = getPriceTmall(url)
+	else :
+		result = '------error----------'
 	print result
 	result_file.write(result + '\n')
 
-url_file.close();
-result_file.close();
+url_file.close()
+result_file.close()
